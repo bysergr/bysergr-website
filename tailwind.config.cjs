70f22ae162801de6a78d68bcf29a6bd6d587dfde
Sergio Rey <reysergio383@gmail.com>
2023-09-17 13:21:45 -0500

    Add font family and name in index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -5,6 +7,20 @@ module.exports = {
     extend: {
       colors: {
         dark: "#111111",
+      },
+    },
+    fontFamily: {
+      display: ['"Source Code Pro"', ...defaultTheme.fontFamily.mono],
+      name: ["Roboto", ...defaultTheme.fontFamily.sans],
+    },
+    animation: {
+      "move-bg": "gradient 10s ease infinite",
+    },
+    keyframes: {
+      gradient: {
+        "0%": { "background-position": "0 50%" },
+        "50%": { "background-position": "100% 50%" },
+        "100%": { "background-position": "0 50%" },
       },
     },
   },
