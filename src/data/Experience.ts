8cbc59e68d30a71fc0ec9ce56ebd2886a21385da
Sergio Rey <reysergio383@gmail.com>
2023-09-17 18:58:08 -0500

    Add name and experiences section

---
+++ src/data/Experience.ts
@@ -0,0 +1,32 @@
+export interface experience {
+    title: string;
+    company: string;
+    location: string;
+    from: string;
+    to: string;
+    description: string;
+}
+
+export const experiences: Array<experience> = [{
+    title: 'Software Engineer',
+    company: 'Google',
+    location: 'Mountain View, CA',
+    from: '2019',
+    to: 'Present',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut eius, facilis nihil fugiat molestiae sit voluptatem similique consequatur, numquam reiciendis.'
+},
+{
+    title: 'Software Engineer',
+    company: 'Google',
+    location: 'Mountain View, CA',
+    from: '2019',
+    to: 'Present',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut eius, facilis nihil fugiat molestiae sit voluptatem similique consequatur, numquam reiciendis.'
+},{
+    title: 'Software Engineer',
+    company: 'Google',
+    location: 'Mountain View, CA',
+    from: '2019',
+    to: 'Present',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut eius, facilis nihil fugiat molestiae sit voluptatem similique consequatur, numquam reiciendis.'
+}]
