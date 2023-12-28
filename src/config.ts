aed7be6ac1361c98b4bccbd342ebf9dc3515b5b3
Sergio Rey <reysergio383@gmail.com>
2023-12-27 19:07:08 -0500

    Add project list screen

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { type Site } from "@/types";
 export const SITE: Site = {
   author: "Sergio R.",
   desc: "Explore my coding journey, projects, and tech insights on this platform dedicated to software development. Join me in shaping the future of technology!",
-  title: "Sergio Rey |> Software Developer",
+  title: "Sergio Rey > Software Developer",
   website: "https://bysergr.com",
   postsPerPage: 5,
   projectsPerPage: 5,
