390d37f7c0305594b8234cacc8f7ff9722cab645
Sergio Rey <reysergio383@gmail.com>
2024-01-05 13:24:27 -0500

    Add sitemap and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://bysergr.com",
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+  ],
   redirects: {
     "/projects/1": {
       destination: "/projects",
